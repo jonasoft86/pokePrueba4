ce9e46ab100e98974f06dbfa81468ffa0be424bf
Jonasoft <jonathan.monroy@live.com>
2024-12-08 13:04:16 -0300

    fix ResultSearch

--- src/pokemons/interfaces/index.ts
+++ src/pokemons/interfaces/index.ts
@@ -1,3 +1,3 @@
 export type { PokemonListResponse } from './pokemon-list.response';
 export type { PokemonResponse } from './pokemon.response';
-export type { Pokemon } from './pokemon';
+export type { Pokemon, ResultSearch } from "./pokemon"
