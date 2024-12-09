33cc006a8c2b0e8f01c44e2367287a3d347fce96
Jonasoft <jonathan.monroy@live.com>
2024-12-09 15:55:38 -0300

    add intercepto

--- src/pokemons/api/pokemonApi.ts
+++ src/pokemons/api/pokemonApi.ts
@@ -1,7 +1,21 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 const pokemonApi = axios.create({
   baseURL: 'https://pokeapi.co/api/v2'
 });
+
+pokemonApi.interceptors.response.use(
+  response => response,
+  (error: AxiosError) => {
+      if (error.response) {
+          console.error(`Error en la respuesta de la API: ${error.response.status}`, error.response.data);
+      } else if (error.request) {
+          console.error('No se recibió respuesta de API', error.request);
+      } else {
+          console.error('Error en la configuración de la solicitud', error.message);
+      }
+      return Promise.reject(error);
+  }
+);
 
 export default pokemonApi;
